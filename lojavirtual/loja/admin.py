8b9eb25c15b9b8d759bd26cde22003e437e400f5
yurih1 <yurih2009@gmail.com>
2023-01-02 14:27:16 -0300

    added Roupa in model

--- lojavirtual/loja/admin.py
+++ lojavirtual/loja/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from loja.models import Roupa
 
-# Register your models here.
+class Roupas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'descricao', 'modelo', 'tamanho')
+    list_display_links = ('id', 'nome')
+    list_per_page = 50
+    search_fields = ('nome', 'modelo',)
+
+admin.site.register(Roupa, Roupas)
--- lojavirtual/loja/models.py
+++ lojavirtual/loja/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Roupa(models.Model):
+    TAMANHO = (
+        ("P", "Pequeno"),
+        ("M", "Médio"),
+        ("G", "Grande"),
+        ("GG", "Extra Grande")
+    )
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=30)
+    tamanho = models.CharField(max_length=2, choices=TAMANHO, blank=False, null=False)
+    
+    def __str__(self):
+        return self.nome
